1829231d67eed3c12d2c73018a426799896c425d
hy310 <haeyounglee97@gmail.com>
2021-06-05 12:43:27 +0900

    toString() 자동변환

---
+++ Java_ch06/src/ToStringEx.java
@@ -0,0 +1,19 @@
+class Point {
+	private int x, y;
+	public Point(int x, int y) {
+		this.x = x; this.y = y;
+	}
+	public String toString() {   // Point 객체를 문자열로
+		return "Point(" + x + "," + y + ")";
+	}
+	
+}
+public class ToStringEx {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Point a = new Point(2,3);
+		System.out.println(a.toString());
+		System.out.println(a);    // a는 a.toString() 으로 자동변환됨
+	}
+}
