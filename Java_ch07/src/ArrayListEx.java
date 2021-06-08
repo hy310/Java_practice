1b44e41c384ac48175fd1baf2cc5dfc8c1f5fef5
hy310 <haeyounglee97@gmail.com>
2021-06-08 16:46:51 +0900

    문자열만 다루는 ArrayList<String> 활용

---
+++ Java_ch07/src/ArrayListEx.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+// 이름을 4개 입력받아 ArrayList에 저장하고, 
+// ArrayList에 저장된 이름을 모두 출력한 후, 제일 긴 이름 출력
+public class ArrayListEx {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		ArrayList<String> a = new ArrayList<String>();
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		for(int i=0; i<4; i++)
+		{
+			System.out.print("이름을 입력하세요");
+			String s = scanner.next();
+			a.add(s);
+		}
+		
+		for(int i=0; i<a.size(); i++)
+		{
+			String name = a.get(i);
+			System.out.print(name+" ");
+		}
+		//
+		int longestIndex = 0;
+		for(int i=0; i<a.size(); i++) {
+			if(a.get(longestIndex).length() < a.get(i).length())
+				longestIndex = i;
+		}
+		
+		System.out.println("가장 긴 이름은 : " + a.get(longestIndex));
+		scanner.close();
+	}
+}
