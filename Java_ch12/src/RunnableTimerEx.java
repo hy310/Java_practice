67e4a9a9a2ad547c684e76d5b80cc39c0c957aca
hy310 <haeyounglee97@gmail.com>
2021-06-10 13:50:40 +0900

    thread 상속받아 타이머 스레드 만들기

---
+++ Java_ch12/src/RunnableTimerEx.java
@@ -0,0 +1,15 @@
+import javax.swing.JFrame;
+
+public class RunnableTimerEx extends JFrame {
+	
+	public RunnableTimerEx() {
+		setTitle("½º·¹µå 2");
+		set
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- Java_ch12/src/ThreadTimerEx.java
+++ Java_ch12/src/ThreadTimerEx.java
@@ -25,7 +25,6 @@ class TimerThread extends Thread {
 				return;
 			}
 		}
-		
 	}
 }
 
